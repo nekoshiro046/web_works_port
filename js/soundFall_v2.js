9948197ae87124c534b919b515b08c1a49e1e760
nekoshiro046 <nekoshiro046@gmail.com>
2019-05-10 18:35:18 +0900

    updata

--- js/soundFall_v2.js
+++ js/soundFall_v2.js
@@ -6,6 +6,8 @@ var hue1,hue2;
 
 var osc,osc2,osc3,osc4;
 var selTim;
+var firstTouch = 0;
+var firstSound;
 
 
 function setup(){
@@ -38,6 +40,16 @@ function updata(){
 
 function draw(){
 	moPr = mouseIsPressed;
+  if(moPr){
+    if(firstTouch == 0){
+      firstSound = new oscil("sine",0);
+      firstSound.playOscil();
+      firstTouch = 1;
+    }else if(firstTouch == 1){
+      firstSound.stopOscil();
+      firstTouch = 2;
+    }
+  }
 
 	updata();
 	soundUpdata();
